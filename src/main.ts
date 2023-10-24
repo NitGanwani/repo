f8e3a1a1b96dca96602781932d084c85990ebca3
Nitin <nitganwani@gmail.com>
2023-10-24 10:55:11 +0200

    Add new indentation

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import './style.css';
 
 document.querySelector<HTMLDivElement>('.app')!.innerHTML = `
-
   <ul class="characters-list row list-unstyled">
     <li class="character col">
       <div class="card character__card">
@@ -26,20 +25,19 @@ document.querySelector<HTMLDivElement>('.app')!.innerHTML = `
               <li>Peloteo: X</li>
               <li>Asesora a: X</li>
               <li>Sirve a: X</li>
-               </ul>
+            </ul>
             <div class="character__actions">
-                 <button class="character__action btn">habla</button>
+              <button class="character__action btn">habla</button>
               <button class="character__action btn">muere</button>
             </div>
           </div>
         </div>
-           <i class="emoji"></i>
-         </div>
+          <i class="emoji"></i>
+        </div>
     </li>
-     </ul>
+  </ul>
     <div class="comunications">
-    <p class="comunications__text display-1">Una frase que dice alguien</p>
-    <img class="comunications__picture" src="img/no-one.jpg" alt="Nombre y familia del que habla" />
+      <p class="comunications__text display-1">Una frase que dice alguien</p>
+      <img class="comunications__picture" src="img/no-one.jpg" alt="Nombre y familia del que habla" />
     </div>
-
 `;
